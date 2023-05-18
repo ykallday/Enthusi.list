428b87c75b9693d7ccaa8690e3b3ab2c45b20770
Yael Kaufman <ylkauf@gmail.com>
2023-05-18 11:36:13 -0700

    testing readme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import {BASE_URL} from './globals'
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([])
+  useEffect(()=>{
+    const getMovies = async() => {
+      const response = await axios.get(`${BASE_URL}/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}`)
+      setMovies (response.data.results)
+      console.log(movies)
+    }
+    getMovies()
+  },[])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
